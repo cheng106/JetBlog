8a1c99315befcd38434e873418d5d5792acfab54
Cheng® <cheng106.andy@gmail.com>
2021-08-13 17:19:02 +0800

    init: JetBlog專案建立

---
+++ src/main/java/com/cheng/jetblog/JetblogApplication.java
@@ -0,0 +1,13 @@
+package com.cheng.jetblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cheng/jetblog/JetblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheng.jetblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
