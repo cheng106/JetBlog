ecc0cb0956dc7a9e07da7f28fcaae687c3dc8a29
Cheng® <cheng106.andy@gmail.com>
2021-08-16 22:59:51 +0800

    refactor: 移動靜態頁面到template

--- src/main/java/com/cheng/jetblog/web/IndexController.java
+++ src/main/java/com/cheng/jetblog/web/IndexController.java
@@ -1,6 +1,5 @@
 package com.cheng.jetblog.web;
 
-import com.cheng.jetblog.exception.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")
+    @GetMapping("/")
     public String index(@PathVariable Integer id, @PathVariable String name) {
         System.out.println("--------index--------");
 //        String blog = null;
