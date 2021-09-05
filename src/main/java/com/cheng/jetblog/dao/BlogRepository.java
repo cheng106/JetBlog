7df4deff8ef2c6707e9c89d1bc9ffdeeeafcaa44
Cheng® <cheng106.andy@gmail.com>
2021-09-05 13:18:39 +0800

    feat: 新增搜尋功能

--- src/main/java/com/cheng/jetblog/dao/BlogRepository.java
+++ src/main/java/com/cheng/jetblog/dao/BlogRepository.java
@@ -1,6 +1,7 @@
 package com.cheng.jetblog.dao;
 
 import com.cheng.jetblog.po.Blog;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -16,4 +17,8 @@ public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificat
 
     @Query("select b from Blog b where b.isRecommend = true")
     List<Blog> findTop(Pageable pageable);
+
+    // select * from t_blog where title like '%內容%'
+    @Query("select b from Blog b where b.title like ?1 or b.content like ?1")
+    Page<Blog> findByQuery(String query, Pageable pageable);
 }
--- src/main/java/com/cheng/jetblog/web/IndexController.java
+++ src/main/java/com/cheng/jetblog/web/IndexController.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author cheng
@@ -35,6 +37,15 @@ public class IndexController {
         model.addAttribute("tags", tagService.listTagTop(10));
         model.addAttribute("recommendBlogList", blogService.ListRecommendBlogTop(8));
         return "index";
+    }
+
+    @PostMapping("/search")
+    public String search(@PageableDefault(size = 5, sort = {"updateTime"}, direction = Sort.Direction.DESC)
+                                 Pageable pageable, @RequestParam String query, Model model) {
+
+        model.addAttribute("page", blogService.listBlog("%" + query + "%", pageable));
+        model.addAttribute("query", query);
+        return "search";
     }
 
     @GetMapping("/blog")
